76b8f170e80b2a581f6dc1f7a9dbde3aeb7eb4ba
nicolefeliceee <nicolefeliceee@gmail.com>
2023-04-28 21:49:00 +0700

    gallery detail done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,74 +25,74 @@ class DatabaseSeeder extends Seeder
         Gallery::create([
             'title' => 'Judul Pertama Coba',
             'slug' => 'title-1',
-            'image' => 'me2.png',
+            'image' => 'title1-1.JPG',
             'status' => 'Kids',
             'yearMoment' => '2005',
-            'story' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In debitis magnam maiores reiciendis illum odio cupiditate ab nemo, excepturi assumenda asperiores fugiat autem nisi incidunt culpa ipsum dicta delectus quia magni voluptate ratione omnis hic. Cumque sunt iste saepe adipisci atque! Nam illum repellat eum exercitationem nobis dicta in officiis?'
+            'story' => '<p style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam accusamus vitae eaque sapiente ad suscipit in, cupiditate fugiat nesciunt soluta voluptatibus tempora consequuntur enim repellat labore, quaerat a dicta officiis quas omnis quis qui adipisci expedita. Praesentium eos nobis assumenda obcaecati aut, natus alias similique ullam earum modi quam facere minima tenetur odit, maxime, architecto magnam ipsa tempora sequi!</p><p style="text-align: justify">Pariatur eveniet sapiente veritatis cumque totam ratione iste quo nobis, blanditiis dolorum aut dolorem, architecto facilis modi doloremque magnam, perferendis aliquid voluptatem corrupti laborum quae non iure unde voluptatum? Illo, ex totam quos temporibus, odit molestias asperiores inventore, cupiditate alias fugit sed placeat quaerat nobis. Voluptatum ut illum iure voluptatibus earum alias ex dolor. Rem, nesciunt impedit iusto atque ducimus quo nobis ullam.</p>'
         ]);
         Gallery::create([
             'title' => 'title 2',
             'slug' => 'title-2',
-            'image' => 'title2-1',
+            'image' => 'title2-1.JPG',
             'status' => 'Teens',
             'yearMoment' => '2010',
-            'story' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In debitis magnam maiores reiciendis illum odio cupiditate ab nemo, excepturi assumenda asperiores fugiat autem nisi incidunt culpa ipsum dicta delectus quia magni voluptate ratione omnis hic. Cumque sunt iste saepe adipisci atque! Nam illum repellat eum exercitationem nobis dicta in officiis?'
+            'story' => '<p style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam accusamus vitae eaque sapiente ad suscipit in, cupiditate fugiat nesciunt soluta voluptatibus tempora consequuntur enim repellat labore, quaerat a dicta officiis quas omnis quis qui adipisci expedita. Praesentium eos nobis assumenda obcaecati aut, natus alias similique ullam earum modi quam facere minima tenetur odit, maxime, architecto magnam ipsa tempora sequi!</p><p style="text-align: justify">Pariatur eveniet sapiente veritatis cumque totam ratione iste quo nobis, blanditiis dolorum aut dolorem, architecto facilis modi doloremque magnam, perferendis aliquid voluptatem corrupti laborum quae non iure unde voluptatum? Illo, ex totam quos temporibus, odit molestias asperiores inventore, cupiditate alias fugit sed placeat quaerat nobis. Voluptatum ut illum iure voluptatibus earum alias ex dolor. Rem, nesciunt impedit iusto atque ducimus quo nobis ullam.</p>'
         ]);
         Gallery::create([
             'title' => 'title 3',
             'slug' => 'title-3',
-            'image' => 'title3-1',
+            'image' => 'title3-1.JPG',
             'status' => 'Adult',
             'yearMoment' => '2023',
-            'story' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In debitis magnam maiores reiciendis illum odio cupiditate ab nemo, excepturi assumenda asperiores fugiat autem nisi incidunt culpa ipsum dicta delectus quia magni voluptate ratione omnis hic. Cumque sunt iste saepe adipisci atque! Nam illum repellat eum exercitationem nobis dicta in officiis?'
+            'story' => '<p style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam accusamus vitae eaque sapiente ad suscipit in, cupiditate fugiat nesciunt soluta voluptatibus tempora consequuntur enim repellat labore, quaerat a dicta officiis quas omnis quis qui adipisci expedita. Praesentium eos nobis assumenda obcaecati aut, natus alias similique ullam earum modi quam facere minima tenetur odit, maxime, architecto magnam ipsa tempora sequi!</p><p style="text-align: justify">Pariatur eveniet sapiente veritatis cumque totam ratione iste quo nobis, blanditiis dolorum aut dolorem, architecto facilis modi doloremque magnam, perferendis aliquid voluptatem corrupti laborum quae non iure unde voluptatum? Illo, ex totam quos temporibus, odit molestias asperiores inventore, cupiditate alias fugit sed placeat quaerat nobis. Voluptatum ut illum iure voluptatibus earum alias ex dolor. Rem, nesciunt impedit iusto atque ducimus quo nobis ullam.</p>'
         ]);
         Gallery::create([
             'title' => 'title 4',
             'slug' => 'title-4',
-            'image' => 'title4-1',
+            'image' => 'title4-1.JPG',
             'status' => 'Teens',
             'yearMoment' => '2010',
-            'story' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In debitis magnam maiores reiciendis illum odio cupiditate ab nemo, excepturi assumenda asperiores fugiat autem nisi incidunt culpa ipsum dicta delectus quia magni voluptate ratione omnis hic. Cumque sunt iste saepe adipisci atque! Nam illum repellat eum exercitationem nobis dicta in officiis?'
+            'story' => '<p style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam accusamus vitae eaque sapiente ad suscipit in, cupiditate fugiat nesciunt soluta voluptatibus tempora consequuntur enim repellat labore, quaerat a dicta officiis quas omnis quis qui adipisci expedita. Praesentium eos nobis assumenda obcaecati aut, natus alias similique ullam earum modi quam facere minima tenetur odit, maxime, architecto magnam ipsa tempora sequi!</p><p style="text-align: justify">Pariatur eveniet sapiente veritatis cumque totam ratione iste quo nobis, blanditiis dolorum aut dolorem, architecto facilis modi doloremque magnam, perferendis aliquid voluptatem corrupti laborum quae non iure unde voluptatum? Illo, ex totam quos temporibus, odit molestias asperiores inventore, cupiditate alias fugit sed placeat quaerat nobis. Voluptatum ut illum iure voluptatibus earum alias ex dolor. Rem, nesciunt impedit iusto atque ducimus quo nobis ullam.</p>'
         ]);
         Gallery::create([
             'title' => 'title 5',
             'slug' => 'title-5',
-            'image' => 'title5-1',
+            'image' => 'title5-1.JPG',
             'status' => 'Kids',
             'yearMoment' => '2005',
-            'story' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In debitis magnam maiores reiciendis illum odio cupiditate ab nemo, excepturi assumenda asperiores fugiat autem nisi incidunt culpa ipsum dicta delectus quia magni voluptate ratione omnis hic. Cumque sunt iste saepe adipisci atque! Nam illum repellat eum exercitationem nobis dicta in officiis?'
+            'story' => '<p style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam accusamus vitae eaque sapiente ad suscipit in, cupiditate fugiat nesciunt soluta voluptatibus tempora consequuntur enim repellat labore, quaerat a dicta officiis quas omnis quis qui adipisci expedita. Praesentium eos nobis assumenda obcaecati aut, natus alias similique ullam earum modi quam facere minima tenetur odit, maxime, architecto magnam ipsa tempora sequi!</p><p style="text-align: justify">Pariatur eveniet sapiente veritatis cumque totam ratione iste quo nobis, blanditiis dolorum aut dolorem, architecto facilis modi doloremque magnam, perferendis aliquid voluptatem corrupti laborum quae non iure unde voluptatum? Illo, ex totam quos temporibus, odit molestias asperiores inventore, cupiditate alias fugit sed placeat quaerat nobis. Voluptatum ut illum iure voluptatibus earum alias ex dolor. Rem, nesciunt impedit iusto atque ducimus quo nobis ullam.</p>'
         ]);
         Gallery::create([
             'title' => 'title 6',
             'slug' => 'title-6',
-            'image' => 'title6-1',
+            'image' => 'title6-1.JPG',
             'status' => 'Teens',
             'yearMoment' => '2010',
-            'story' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In debitis magnam maiores reiciendis illum odio cupiditate ab nemo, excepturi assumenda asperiores fugiat autem nisi incidunt culpa ipsum dicta delectus quia magni voluptate ratione omnis hic. Cumque sunt iste saepe adipisci atque! Nam illum repellat eum exercitationem nobis dicta in officiis?'
+            'story' => '<p style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam accusamus vitae eaque sapiente ad suscipit in, cupiditate fugiat nesciunt soluta voluptatibus tempora consequuntur enim repellat labore, quaerat a dicta officiis quas omnis quis qui adipisci expedita. Praesentium eos nobis assumenda obcaecati aut, natus alias similique ullam earum modi quam facere minima tenetur odit, maxime, architecto magnam ipsa tempora sequi!</p><p style="text-align: justify">Pariatur eveniet sapiente veritatis cumque totam ratione iste quo nobis, blanditiis dolorum aut dolorem, architecto facilis modi doloremque magnam, perferendis aliquid voluptatem corrupti laborum quae non iure unde voluptatum? Illo, ex totam quos temporibus, odit molestias asperiores inventore, cupiditate alias fugit sed placeat quaerat nobis. Voluptatum ut illum iure voluptatibus earum alias ex dolor. Rem, nesciunt impedit iusto atque ducimus quo nobis ullam.</p>'
         ]);
         Gallery::create([
             'title' => 'title 7',
             'slug' => 'title-7',
-            'image' => 'title7-1',
+            'image' => 'title7-1.JPG',
             'status' => 'Adult',
             'yearMoment' => '2023',
-            'story' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In debitis magnam maiores reiciendis illum odio cupiditate ab nemo, excepturi assumenda asperiores fugiat autem nisi incidunt culpa ipsum dicta delectus quia magni voluptate ratione omnis hic. Cumque sunt iste saepe adipisci atque! Nam illum repellat eum exercitationem nobis dicta in officiis?'
+            'story' => '<p style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam accusamus vitae eaque sapiente ad suscipit in, cupiditate fugiat nesciunt soluta voluptatibus tempora consequuntur enim repellat labore, quaerat a dicta officiis quas omnis quis qui adipisci expedita. Praesentium eos nobis assumenda obcaecati aut, natus alias similique ullam earum modi quam facere minima tenetur odit, maxime, architecto magnam ipsa tempora sequi!</p><p style="text-align: justify">Pariatur eveniet sapiente veritatis cumque totam ratione iste quo nobis, blanditiis dolorum aut dolorem, architecto facilis modi doloremque magnam, perferendis aliquid voluptatem corrupti laborum quae non iure unde voluptatum? Illo, ex totam quos temporibus, odit molestias asperiores inventore, cupiditate alias fugit sed placeat quaerat nobis. Voluptatum ut illum iure voluptatibus earum alias ex dolor. Rem, nesciunt impedit iusto atque ducimus quo nobis ullam.</p>'
         ]);
         Gallery::create([
             'title' => 'title 8',
             'slug' => 'title-8',
-            'image' => 'title8-1',
+            'image' => 'title8-1.JPG',
             'status' => 'Teens',
             'yearMoment' => '2010',
-            'story' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In debitis magnam maiores reiciendis illum odio cupiditate ab nemo, excepturi assumenda asperiores fugiat autem nisi incidunt culpa ipsum dicta delectus quia magni voluptate ratione omnis hic. Cumque sunt iste saepe adipisci atque! Nam illum repellat eum exercitationem nobis dicta in officiis?'
+            'story' => '<p style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam accusamus vitae eaque sapiente ad suscipit in, cupiditate fugiat nesciunt soluta voluptatibus tempora consequuntur enim repellat labore, quaerat a dicta officiis quas omnis quis qui adipisci expedita. Praesentium eos nobis assumenda obcaecati aut, natus alias similique ullam earum modi quam facere minima tenetur odit, maxime, architecto magnam ipsa tempora sequi!</p><p style="text-align: justify">Pariatur eveniet sapiente veritatis cumque totam ratione iste quo nobis, blanditiis dolorum aut dolorem, architecto facilis modi doloremque magnam, perferendis aliquid voluptatem corrupti laborum quae non iure unde voluptatum? Illo, ex totam quos temporibus, odit molestias asperiores inventore, cupiditate alias fugit sed placeat quaerat nobis. Voluptatum ut illum iure voluptatibus earum alias ex dolor. Rem, nesciunt impedit iusto atque ducimus quo nobis ullam.</p>'
         ]);
         Gallery::create([
             'title' => 'title 9',
             'slug' => 'title-9',
-            'image' => 'title9-1',
+            'image' => 'title9-1.JPG',
             'status' => 'Teens',
             'yearMoment' => '2010',
-            'story' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. In debitis magnam maiores reiciendis illum odio cupiditate ab nemo, excepturi assumenda asperiores fugiat autem nisi incidunt culpa ipsum dicta delectus quia magni voluptate ratione omnis hic. Cumque sunt iste saepe adipisci atque! Nam illum repellat eum exercitationem nobis dicta in officiis?'
+            'story' => '<p style="text-align: justify">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ipsam accusamus vitae eaque sapiente ad suscipit in, cupiditate fugiat nesciunt soluta voluptatibus tempora consequuntur enim repellat labore, quaerat a dicta officiis quas omnis quis qui adipisci expedita. Praesentium eos nobis assumenda obcaecati aut, natus alias similique ullam earum modi quam facere minima tenetur odit, maxime, architecto magnam ipsa tempora sequi!</p><p style="text-align: justify">Pariatur eveniet sapiente veritatis cumque totam ratione iste quo nobis, blanditiis dolorum aut dolorem, architecto facilis modi doloremque magnam, perferendis aliquid voluptatem corrupti laborum quae non iure unde voluptatum? Illo, ex totam quos temporibus, odit molestias asperiores inventore, cupiditate alias fugit sed placeat quaerat nobis. Voluptatum ut illum iure voluptatibus earum alias ex dolor. Rem, nesciunt impedit iusto atque ducimus quo nobis ullam.</p>'
         ]);
     }
 }
--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -8,19 +8,34 @@
 
 @section('content')
     <div class="allContainer gallery">
-        <div class="section-title">
-            <h2>Gallery</h2>
-            <p>A slice of my life</p>
-        </div>
-        <div class="card" style="background-color:
-            {{ $gallery->status == "Kids"? 'yellow' : '' }}
-            {{ $gallery->status == "Teens"? 'pink' : '' }}
-            {{ $gallery->status == "Adult"? 'green' : '' }}
-        ">
-            <img src="{{ asset('sl3/img/' . $gallery->image) }}" alt="{{ $gallery->title }}">
-            <h1>{{ $gallery->title }}</h1>
-            <h3>{{ $gallery->status }} - {{ $gallery->yearMoment }}</h3>
-            <p>{{ $gallery->story }}</p>
+        <div class="card mb-3 w-100">
+            <div class="row g-0">
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h3 class="card-title m-0 p-0" style="color:
+                            {{ $gallery->status == "Kids"? 'rgb(205,37,251,.7)' : '' }}
+                            {{ $gallery->status == "Teens"? 'rgb(251,165,37,.7)' : '' }}
+                            {{ $gallery->status == "Adult"? 'rgb(37,155,251,.7)' : '' }}
+                            ">{{ $gallery->title }}</h3>
+                        <p class="card-text"><small class="text-body-secondary">{{ $gallery->status }} - {{ $gallery->yearMoment }}</small></p>
+                        <p class="card-text">{!! $gallery->story !!}</p>
+                        <a href="/galleries" class="btn btn-primary" style="background-color:
+                            {{ $gallery->status == "Kids"? 'rgb(205,37,251,.7)' : '' }}
+                            {{ $gallery->status == "Teens"? 'rgb(251,165,37,.7)' : '' }}
+                            {{ $gallery->status == "Adult"? 'rgb(37,155,251,.7)' : '' }}
+                        ">Back</a>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="img-color" style="background-color:
+                        {{ $gallery->status == "Kids"? 'rgb(205,37,251,.7)' : '' }}
+                        {{ $gallery->status == "Teens"? 'rgb(251,165,37,.7)' : '' }}
+                        {{ $gallery->status == "Adult"? 'rgb(37,155,251,.7)' : '' }}
+                    ">
+                        <div class="img-fluid" style="background-image: url({{ asset('sl3/img/' . $gallery->image) }})"></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 @endsection
